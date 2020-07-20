7f0a51ced075feb47ffbd226bb87bf26f763e62f
Dan Dalgatov <dandalgatov@Dans-MacBook-Air.local>
2020-07-20 14:54:04 -0400

    fixed association

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,3 +1,3 @@
 class Neighborhood < ApplicationRecord
-    has_many :listings
+    has_many :listings, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Neighborhood.destroy_all
+
 Image.destroy_all
 Listing.destroy_all
+Neighborhood.destroy_all
+
 Amenity.destroy_all
 User.destroy_all
 
