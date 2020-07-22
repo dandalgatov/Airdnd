0d6e15bf19472b1efd3321db268a4bffaa5e8436
Dan Dalgatov <dandalgatov@Dans-MacBook-Air.local>
2020-07-22 03:10:44 -0400

    fixed the delete image logic, and create new image on edited listing logic

--- client/src/components/AddImage/index.jsx
+++ client/src/components/AddImage/index.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import { Segment, Form, Card, Icon, Button, Divider } from 'semantic-ui-react'
+import { deleteImage, createImage } from '../../services/api'
 
 export default function AddImage(props) {
 
-    const { activeImages, setActiveImages} = props
+    const { activeImages, setActiveImages, listing_id} = props
     const [newImage, setNewImage] = useState({})
 
 
@@ -15,19 +16,22 @@ export default function AddImage(props) {
     }
 
     const handleNewImageSubmit = (e, data) => {
-        if (newImage.imageUrl) {
+        if (newImage.url) {
             setActiveImages([
                 ...activeImages, newImage
             ])
-            setNewImage({ imageUrl: '' })
+            if (listing_id) {
+                createImage(listing_id, newImage)
+            }
+            setNewImage({ url: '' })
         } else {
             alert("Must Enter URL before adding an Image")
         }
-        
     }
 
     const handleDeleteImage = (index) => {
-        activeImages.splice(index, 1)
+        const imageToDelete = activeImages.splice(index, 1)
+        deleteImage(imageToDelete[0].id)
         setActiveImages([...activeImages])
         console.log(activeImages)
     }
@@ -47,18 +51,19 @@ export default function AddImage(props) {
                         actionPosition='right'
                         placeholder='https://'
                         width={16}
-                        name='imageUrl'
-                        value={newImage.imageUrl}
+                        name='url'
+                        value={newImage.url}
                         onChange={handleNewImageChange}
                     />
                 </Form.Group>
             </Form>
-            <Divider />
+            <Divider/>
+            
             <Card.Group itemsPerRow={2} doubling>
                 {activeImages && activeImages.map((image, index) => {
                     return <Card
                         stackable
-                        image={image.imageUrl}
+                        image={image.url}
                         extra={<div className='ui three buttons'>
                             <Button basic color='red' onClick={() => handleDeleteImage(index)}>
                                 <Icon name='trash' />
