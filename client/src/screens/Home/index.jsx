170fadb8fb5e0c738fd34446aac216cdf259c682
Dan Dalgatov <dandalgatov@Dans-MacBook-Air.local>
2020-07-22 06:18:41 -0400

    git hook test

--- client/src/screens/Home/index.jsx
+++ client/src/screens/Home/index.jsx
@@ -32,11 +32,10 @@ export default function Home(props) {
     return (
         <main>
             <div style={{ height: "100vh" }}>
-                {/* <BasicSearch
+                <BasicSearch
                     neighborhoodOptions={neighborhoods}
                     setBasicSearchResults={setBasicSearchResults}
-                /> */}
-                {/* gf */}
+                />
                 <Segment >
                     < Carousel
                         autoPlay
