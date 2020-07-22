7451b2e6fa4e24d90b706da26cd5b7a0860233e9
Dan Dalgatov <dandalgatov@Dans-MacBook-Air.local>
2020-07-21 21:11:01 -0400

    added try/catch to api file

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,66 +2,114 @@ import api from './apiconfig'
 
 //Users
 export const getUser = async (id) => {
-    const response = await api.get(`/users/${id}`)
-    return response.data
+    try {
+        const response = await api.get(`/users/${id}`)
+        return response.data
+    } catch (error) {
+        throw error
+    }
 }
 
 export const updateUser = async (id, userData) => {
-    const response = await api.put(`/users/${id}`, { user: userData })
-    return response.data
+    try {
+        const response = await api.put(`/users/${id}`, { user: userData })
+        return response.data
+    } catch (error) {
+        throw error
+    }
 }
 
 //Listings
 export const getListing = async (id) => {
-    const response = await api.get(`/listings/${id}`)
-    return response.data
+    try {
+        const response = await api.get(`/listings/${id}`)
+        return response.data
+    } catch (error) {
+        throw error
+    }
 }
 
 export const getUserListings = async (id) => {
-    const response = await api.get(`/users/${id}/listings`)
-    return response.data
+    try {
+        const response = await api.get(`/users/${id}/listings`)
+        return response.data
+    } catch (error) {
+        throw error
+    }
 }
 
 export const createListing = async (listingData) => {
-    const response = await api.post('/listings', listingData)
-    return response.data
+    try {
+        const response = await api.post('/listings', listingData)
+        return response.data
+    } catch (error) {
+        throw error
+    }
 }
 
 export const editListing = async (id, listingData) => {
-    const response = await api.put(`/listings/${id}`, listingData)
-    return response.data
+    try {
+        const response = await api.put(`/listings/${id}`, listingData)
+        return response.data
+    } catch (error) {
+        throw error
+    }
 }
 
 export const deleteListing = async (id) => {
-    const response = await api.delete(`/listings/${id}`)
-    return response.data
+    try {
+        const response = await api.delete(`/listings/${id}`)
+        return response.data
+    } catch (error) {
+        throw error
+    }
 }
 
 // Images
 export const getImages = async (listing_id) => {
-    const response = await api.get(`/listings/${listing_id}/images`)
-    return response.data
+    try {
+        const response = await api.get(`/listings/${listing_id}/images`)
+        return response.data
+    } catch (error) {
+        throw error
+    }
 }
 
 export const createImage = async (listing_id, url) => {
-    const response = await api.post(`/listings/${listing_id}/images`, {images: url})
-    return response.data
+    try {
+        const response = await api.post(`/listings/${listing_id}/images`, { images: url })
+        return response.data
+    } catch (error) {
+        throw error
+    }
 }
 
 //Amenities
 export const getAmenities = async () => {
-    const response = await api.get('/amenities')
-    return response.data
+    try {
+        const response = await api.get('/amenities')
+        return response.data
+    } catch (error) {
+        throw error
+    }
 }
 
 //Neighborhoods
 export const getNeighborhoods = async () => {
-    const response = await api.get('/neighborhoods')
-    return response.data
+    try {
+        const response = await api.get('/neighborhoods')
+        return response.data
+    } catch (error) {
+        throw error
+    }
 }
 
 //Search Results
 export const getSearchResults = async (searchParams) => {
-    const response = await api.post('/listings/search', searchParams)
-    return response.data
+    try {
+        const response = await api.post('/listings/search', searchParams)
+        return response.data
+    } catch (error) {
+        throw error
+    }
 }
